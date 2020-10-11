f08f65d08d584802eced42dd3a0ae5e934e5da04
Oscar Loras Delgado <oscar.loras.delgado@everis.com>
2020-10-11 13:15:59 +0200

    sustitucion de todos los strings lirevenas por hab_sonda para hacer coherente la configuracion de todos los archivos

--- ConfigHelper/ConfigHelperUtil.py
+++ ConfigHelper/ConfigHelperUtil.py
@@ -5,7 +5,7 @@ import logging
 
 #loggerLog = logging.getLogger('server_logger-config')
 #loggerLog.setLevel(logging.INFO)
-#inf = logging.FileHandler('/data/lirevenas/logs/wsp-Config.log')
+#inf = logging.FileHandler('/data/hab_sonda/logs/wsp-Config.log')
 #inf.setLevel(logging.INFO)
 #formatterInformer = logging.Formatter('[%(asctime)s][%(levelname)s][%(message)s]', datefmt='%Y-%m-%d %H:%M:%S')
 #inf.setFormatter(formatterInformer)
--- mpuService.py
+++ mpuService.py
@@ -19,7 +19,7 @@ logger.addHandler(fh)
 #Creacion del logger para los logs de aplicacion
 loggerLog = logging.getLogger('server_logger1')
 loggerLog.setLevel(logging.INFO)
-inf = logging.FileHandler('/data/lirevenas/logs/mpuService.log')
+inf = logging.FileHandler('/data/hab_sonda/logs/mpuService.log')
 inf.setLevel(logging.INFO)
 formatterInformer = logging.Formatter('[%(asctime)s][%(levelname)s][%(message)s]', datefmt='%Y-%m-%d %H:%M:%S')
 inf.setFormatter(formatterInformer)
