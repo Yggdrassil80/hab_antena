42e6c13d27aa59ed99d68cf2f921456cb2c52a5d
Yggdrassil80 <pi@pi.com>
2020-11-01 18:20:51 +0000

    multiples errores de identacion resueltos

--- GPSHelper/GPSHelperUtil.py
+++ GPSHelper/GPSHelperUtil.py
@@ -1,12 +1,11 @@
 
-#!/usr/bin/python
-
-import ConfigParser
 import logging
 import serial
 import time
 import sys
 import os
+
+import ConfirParser
 
 #Creacion del logger para los logs de aplicacion
 loggerLog = logging.getLogger('server_logger_gps') 
@@ -80,36 +79,36 @@ def getGPSData(ser):
 		#loggerLog.error("[GPSHelper][getGPSData] ERROR se retorna objeto invalido:" + e.args[0])
 		return gpsData
 #def setGPSFlyMode(port):
-#	ser = serial.Serial(port, baudrate = 9600, timeout = 0.5) data = 0xB5, 0x62, 
-#	0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 
-#	0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 
-#	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+#	ser = serial.Serial(port, baudrate = 9600, timeout = 0.5) data = 0xB5, 0x62,
+#	0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10,
+#	0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01,
+#	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 #	0x00, 0x16, 0xDC"
 
-#metodo que se encarga de parsear los datos brutos de la mensajeria NMEA entrante por 
+#metodo que se encarga de parsear los datos brutos de la mensajeria NMEA entrante por
 #puerto serie
 def parseGPS_GGA(data):
 	gpsData = [int(0), float(0), float(0), "KO"]
 	try:
 		#loggerLog.debug("[GPSHelper][parseGPS_GGA] Inicio")
 		# $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
-        	#     GGA Global Positioning System Fix Data 123519 Fix taken at 12:35:19 UTC 
-        	#     4807.038,N Latitude 48 deg 07.038' N 01131.000,E Longitude 11 deg 31.000' 
+        	#     GGA Global Positioning System Fix Data 123519 Fix taken at 12:35:19 UTC
+        	#     4807.038,N Latitude 48 deg 07.038' N 01131.000,E Longitude 11 deg 31.000'
         	#     E 1 Fix quality: 0 = invalid
-        	#               1 = GPS fix (SPS) 2 = DGPS fix 3 = PPS fix 4 = Real Time 
+        	#               1 = GPS fix (SPS) 2 = DGPS fix 3 = PPS fix 4 = Real Time
         	#              Kinematic 5 = Float RTK
-        	#               6 = estimated (dead reckoning) (2.3 feature) 7 = Manual input 
-        	#              mode 8 = Simulation mode 08 Number of satellites being tracked 
-        	#              0.9 Horizontal dilution of position 545.4,M Altitude, Meters, 
-        	#              above mean sea level 46.9,M Height of geoid (mean sea level) 
+        	#               6 = estimated (dead reckoning) (2.3 feature) 7 = Manual input
+        	#              mode 8 = Simulation mode 08 Number of satellites being tracked
+        	#              0.9 Horizontal dilution of position 545.4,M Altitude, Meters,
+        	#              above mean sea level 46.9,M Height of geoid (mean sea level)
         	#              above WGS84
-        	#                      ellipsoid (empty field) time in seconds since last DGPS 
-        	#     update (empty field) DGPS station ID number *47 the checksum data, always 
+        	#                      ellipsoid (empty field) time in seconds since last DGPS
+        	#     update (empty field) DGPS station ID number *47 the checksum data, always
         	#     begins with *
 		if data[0:6] == "$GPGGA":
 			#loggerLog.debug("[GPSHelper][parseGPS_GGA] Mensaje $GPGGA encontrado")
-        		s = data.split(",")
-        		if s[7] == '0':
+			s = data.split(",")
+			if s[7] == '0':
 				#loggerLog.debug("[GPSHelper][parseGPS_GGA] No hay satelites disponibles.")
 				return
 			#loggerLog.debug("[GPSHelper][parseGPS_GGA] Satelites disponibles. Detectando campos vacios...")
@@ -117,20 +116,20 @@ def parseGPS_GGA(data):
 			if ((s[2] == '') and (s[4] == '') and (s[9] == '')):
 				#loggerLog.debug("[GPSHelper][parseGPS_GGA] Campos vacios... devolvemos un KO")
 				gpsData[0] = int(0)
-	                	gpsData[1] = float(0)
-        	        	gpsData[2] = float(0)
-                		gpsData[3] = "KO"
+				gpsData[1] = float(0)
+				gpsData[2] = float(0)
+				gpsData[3] = "KO"
 				return gpsData
 			#loggerLog.debug("[GPSHelper][parseGPS_GGA] Datos validos.Recuperando...")
 			time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
-        		lat = decode(s[2])
-        		dirLat = s[3]
-        		lon = decode(s[4])
-        		dirLon = s[5]
+			lat = decode(s[2])
+			dirLat = s[3]
+			lon = decode(s[4])
+			dirLon = s[5]
 			if dirLon == 'W':
 				lon = lon * -1
-        		alt = s[9]
-        		sat = s[7]
+			alt = s[9]
+			sat = s[7]
 			gpsData[0] = alt
 			gpsData[1] = lat
 			gpsData[2] = lon
@@ -138,16 +137,16 @@ def parseGPS_GGA(data):
 		else:
 			#loggerLog.debug("[GPSHelper][parseGPS_GGA] Mensaje $GPGGA NO encontrado. Salir con KO")
 			gpsData[0] = int(0)
-                	gpsData[1] = float(0)
-                	gpsData[2] = float(0)
-                	gpsData[3] = "KO"
+			gpsData[1] = float(0)
+			gpsData[2] = float(0)
+			gpsData[3] = "KO"
 		#loggerLog.info("[GPSHelper][parseGPS_GGA] Fin [0: " + str(gpsData[0]) + "][1: " + str(gpsData[1]) + "][2:" + str(gpsData[2]) + " ][3:" + str(gpsData[3]) + " ]")
 		return gpsData
 	except Exception:
 		e = sys.exc_info()[1]
-    		print(e.args[0])
+		print(e.args[0])
 		#loggerLog.error("[GPSHelper][parseGPS] ERROR se retorna objeto invalido: " + e.args[0])
-                return gpsData
+		return gpsData
 
 #Metodo que transforma el formato de la coordenada
 def decode(coord):
@@ -159,10 +158,10 @@ def decode(coord):
 		if coord != '':
 			#print("[decode][cood: " + coord + "]")
 			v = coord.split(".")
-    			head = v[0]
-    			tail = v[1]
-    			deg = head[0:-2]
-    			min = head[-2:]
+			head = v[0]
+			tail = v[1]
+			deg = head[0:-2]
+			min = head[-2:]
 			resto = float(str(min)+"."+str(tail))
 			#print("resto:"+str(resto))
 			grados = float(str(deg))
@@ -174,19 +173,19 @@ def decode(coord):
 		else:
 			coordenada = float(0)
 			#loggerLog.debug("[GPSHelper][decode] Fin")
-    		return coordenada
+		return coordenada
 	except Exception:
 		e = sys.exc_info()[1]
-                print(e.args[0])
+		print(e.args[0])
 		#loggerLog.error("[GPSHelper][decode] ERROR se retorna coordenada a 0: " + e.args[0])
-        	return coordenada
+		return coordenada
 
 #Metodo que recupera los datos basicos de posicion y altura del GPS
 def parseGPS_RMC(data):
 
-        gpsDataExtendet = [int(0), float(0), float(0), "", "", "", "KO"]
+	gpsDataExtendet = [int(0), float(0), float(0), "", "", "", "KO"]
 
-        try:
+	try:
                 #loggerLog.debug("[GPSHelper][parseGPS_RMC] Inicio")
                 #$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
                 #     RMC          Recommended Minimum sentence C
@@ -204,48 +203,48 @@ def parseGPS_RMC(data):
 		date = "No valido"
 		speed = "No valido"
 
-                if  data[0:6] == "$GPRMC":
-                        loggerLog.debug("[GPSHelper][parseGPS_RMC] Mensaje $GPRMC encontrado")
-                        s = data.split(",")
-                        if ((s[1] == '') or (s[3] == '') or (s[4] == '') or (s[5] == '') or (s[6] == '') or (s[7] == '') or (s[9] == '')):
+		if  data[0:6] == "$GPRMC":
+			loggerLog.debug("[GPSHelper][parseGPS_RMC] Mensaje $GPRMC encontrado")
+			s = data.split(",")
+			if ((s[1] == '') or (s[3] == '') or (s[4] == '') or (s[5] == '') or (s[6] == '') or (s[7] == '') or (s[9] == '')):
                                 #loggerLog.debug("[GPSHelper][gpsDataExtendet] Datos insufucientes para componer traza")
-                                return gpsDataExtendet
-                        loggerLog.debug("[GPSHelper][parseGPS_RMC] Datos validos. Recuperando...")
-                        time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
-                        lat = decode(s[3])
-                        dirLat = s[4]
-                        lon = decode(s[5])
-                        dirLon = s[6]
+				return gpsDataExtendet
+			loggerLog.debug("[GPSHelper][parseGPS_RMC] Datos validos. Recuperando...")
+			time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
+			lat = decode(s[3])
+			dirLat = s[4]
+			lon = decode(s[5])
+			dirLon = s[6]
 
-                        if dirLon == 'W':
-                                lon = lon * -1
+			if dirLon == 'W':
+				lon = lon * -1
 
-                        speed = s[7]
-                        date = s[9][0:2] + "-" + s[9][2:4] + "-" + s[9][4:6]
-                        gpsDataExtendet[1] = lat
-                        gpsDataExtendet[2] = lon
-                        gpsDataExtendet[3] = time
-                        gpsDataExtendet[4] = date
+			speed = s[7]
+			date = s[9][0:2] + "-" + s[9][2:4] + "-" + s[9][4:6]
+			gpsDataExtendet[1] = lat
+			gpsDataExtendet[2] = lon
+			gpsDataExtendet[3] = time
+			gpsDataExtendet[4] = date
 			#Conversion de nudos a m/s
-                        gpsDataExtendet[5] = (float(speed)*float(0.514))
-                        gpsDataExtendet[6] = "OK"
-                else:
-                        loggerLog.debug("[GPSHelper][parseGPS_RMC] Mensaje $GPRMC NO encontrado. Salir con KO")
-                        gpsDataExtendet[0] = int(0)
-                        gpsDataExtendet[1] = float(0)
-                        gpsDataExtendet[2] = float(0)
-                        gpsDataExtendet[3] = ""
-                        gpsDataExtendet[4] = ""
-                        gpsDataExtendet[5] = float(0)
-                        gpsDataExtendet[6] = "KO"
+			gpsDataExtendet[5] = (float(speed)*float(0.514))
+			gpsDataExtendet[6] = "OK"
+		else:
+			loggerLog.debug("[GPSHelper][parseGPS_RMC] Mensaje $GPRMC NO encontrado. Salir con KO")
+			gpsDataExtendet[0] = int(0)
+			gpsDataExtendet[1] = float(0)
+			gpsDataExtendet[2] = float(0)
+			gpsDataExtendet[3] = ""
+			gpsDataExtendet[4] = ""
+			gpsDataExtendet[5] = float(0)
+			gpsDataExtendet[6] = "KO"
 
-                loggerLog.info("[GPSHelper][parseGPS_RMC] Fin [0: " + str(gpsDataExtendet[0]) + "][1: " + str(gpsDataExtendet[1]) + "][2:" + str(gpsDataExtendet[2]) + " ][3:" + str(gpsDataExtendet[3]) + "][4:" + time + "][5:" + date + "][6:" + speed)
-                return gpsDataExtendet
+		loggerLog.info("[GPSHelper][parseGPS_RMC] Fin [0: " + str(gpsDataExtendet[0]) + "][1: " + str(gpsDataExtendet[1]) + "][2:" + str(gpsDataExtendet[2]) + " ][3:" + str(gpsDataExtendet[3]) + "][4:" + time + "][5:" + date + "][6:" + speed)
+		return gpsDataExtendet
 
-        except Exception:
-                e = sys.exc_info()[1]
-                print(e.args[0])
-                loggerLog.error("[GPSHelper][parseGPS_RMC] ERROR se retorna objeto invalido: " + e.args[0])
+	except Exception:
+		e = sys.exc_info()[1]
+		print(e.args[0])
+		loggerLog.error("[GPSHelper][parseGPS_RMC] ERROR se retorna objeto invalido: " + e.args[0])
 		return gpsDataExtendet
 
 
