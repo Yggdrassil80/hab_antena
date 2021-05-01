457f9be0f34bc8bf76f12ed3f436ad10d8e3ef9e
Oscar Loras <oscar.loras@gmail.com>
2021-05-01 13:23:30 +0100

    correcciones para asegurar el correcto arranque de la libreria de envio de datos al mapa en cloud

--- habMapService.sh
+++ habMapService.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 export HABLIB_LOGLEVEL=DEBUG #INFO,ERROR
 export HABLIB_FORMAT="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 export HABLIB_LOGFILE="/data/hab_antena/logs/hablibclient.log"
 
-python3 -m habmapslib.cli --conffile /data/hab_antena/conf/mqttClient.yaml
+/usr/bin/python3 -m habmapslib.cli --conffile /data/hab_antena/conf/mqttClient.yaml
